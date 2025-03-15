7ae0051a7f5665ab94543707151327dc2695a1bd
wind <windz7z@winddeMacBook-Air.local>
2025-03-15 19:04:01 +0545

    Updated app.py with latest changes

--- app.py
+++ app.py
@@ -213,5 +213,7 @@ def serve_static(filename):
     return send_from_directory('static', filename)
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway 提供的端口
+    app.run(host="0.0.0.0", port=port, debug=True)
